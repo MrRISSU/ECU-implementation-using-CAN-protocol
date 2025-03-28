f3dd8452e043a0ed25eba69612722c9a0b0acf50
Huwairis Ibnu Kabeer <127963681+MrRISSU@users.noreply.github.com>
2025-03-28 20:48:46 +0530

    Update DELAY.c

--- DELAY.c
+++ DELAY.c
@@ -1,10 +1,10 @@
 #include<lpc21xx.h>
 void delay_ms(int ms)
 {
-		  T0PR=60000-1;
-		  T0PC=0;
-		  T0TC=0;
-		  T0TCR=1;
-		  while(T0TC<ms);
-		  T0TCR=0;
+    T0PR = 60000 - 1;
+    T0PC = 0;
+    T0TC = 0;
+    T0TCR = 1;
+    while(T0TC < ms);
+    T0TCR = 0;
 }
